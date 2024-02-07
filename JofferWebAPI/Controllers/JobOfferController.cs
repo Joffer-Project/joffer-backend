e2626d9ead691e7c7cd534212d458f396568b52a
Gijs Verdonk <g.verdonk@icloud.com>
2024-02-07 15:31:28 +0200

    Added job offer controller with hardcoded data

---
+++ JofferWebAPI/Controllers/JobOfferController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JofferWebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class JobOfferController : ControllerBase
+{
+    private static readonly string[] JobOffers = new[]
+    {
+        "IT consultant", "Java developer", "C# developer"
+    };
+
+    private readonly ILogger<JobOfferController> _logger;
+
+    public JobOfferController(ILogger<JobOfferController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("GetAll")]
+    public IEnumerable<string> Get()
+    {
+        return JobOffers;
+    }
+}
+
