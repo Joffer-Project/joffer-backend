055505611d39b84a5a970b4d1c426896a4ef679d
Gijs Verdonk <g.verdonk@icloud.com>
2024-03-14 15:31:26 +0100

    Update controller.

--- JofferWebAPI/Controllers/TestController.cs
+++ JofferWebAPI/Controllers/TestController.cs
@@ -15,7 +15,7 @@ namespace JofferWebAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "TestA", "Jippie" };
         }
 
         // GET: api/Test/5
