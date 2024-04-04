51d2bd64ea616c1fb75a4060265ed4b12799006b
Ouden,Silvan S. den <silvan.denouden@student.fontys.nl>
2024-04-04 14:44:07 +0200

    test

--- JofferWebAPI/Controllers/AccountController.cs
+++ JofferWebAPI/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace JofferWebAPI.Controllers
 
         // GET: api/Account
         [HttpGet]
-        [Authorize(Roles = "Talent")]
+        //[Authorize(Roles = "Talent")]
         public async Task<ActionResult<IEnumerable<AccountDto>>> GetAccounts()
         {
           if (_context.Accounts == null)
